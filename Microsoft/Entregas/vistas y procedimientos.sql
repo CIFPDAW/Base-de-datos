6baabeb854698fa213961400ce95c66170b52a14
DanoGlez <fdariasglez@hotmail.com>
2025-08-19 13:47:52 +0100

    Adds stored procedures and view for data retrieval
    Implements a stored procedure to determine the MVP of a match based on statistical calculations.
    
    Creates a view to list referees, teams, and dates of matches.
    
    Adds stored procedures for:
    - Retrieving provinces with more than a specified number of teams.
    - Displaying players and their signing dates for a given team.
    - Showing player statistics for a specific match, including error handling for missing match codes.

--- Microsoft/Entregas/vistas y procedimientos.sql
+++ Microsoft/Entregas/vistas y procedimientos.sql
@@ -36,6 +36,15 @@ exec pa_equiposJugadores;
 -- f. Recuperación = + 1 x recuperación
 -- Ejemplo: Si Sergio Rodríguez contra el Gran Canaria tiene: 20 puntos, 3 faltas cometidas, 2 recibidas, 1 asistencia, 2 pérdidas y 3 recuperaciones hace una valoración de: 20 – 3 + 1 + 1 – 2 + 3 = 20. (Poner un ejemplo de su ejecución)
 
+CREATE OR ALTER PROC pa_mvp
+	    @partido int
+    AS 
+        SELECT Top 1 jp.jugador, j.nombre, (jp.puntos * 1) + (jp.faltascometidas + -1) + (jp.faltasrecibidas * 0.5) + (jp.asistencias * 1) + (jp.perdidas * -1) + (jp.recuperacion * 1) mvp
+        FROM jugadorpartido jp INNER JOIN jugador j
+        ON jp.jugador = j.id_jugador
+        where jp.partido = @partido
+        order by mvp DESC
+
 -- 4. Cree una vista que guarde las fechas, equipos y árbitros ordenador por fecha.
 
 CREATE OR ALTER VIEW vi_Arbitros AS
@@ -43,8 +52,9 @@ SELECT p.fecha, e1.nombre equipo_local, e2.nombre equipo_visitante, a.nombre arb
 FROM partido p
 INNER JOIN equipo e1 ON p.local = e1.id_equipo
 INNER JOIN equipo e2 ON p.visitante = e2.id_equipo
-INNER JOIN arbitro a ON p.arbitro = a.id_arbitro
-ORDER BY p.fecha;
+INNER JOIN arbitro a ON p.arbitro = a.id_arbitro;
+
+SELECT * FROM vi_Arbitros ORDER BY fecha;
 
 -- 5. Cree un proc. Almacenado para que muestre las provincias con más de cinco equipos.
 
@@ -53,11 +63,39 @@ CREATE OR ALTER PROCEDURE pa_provinciasMasCincoEquipos
         SELECT provincia, COUNT(*) total_equipos
         FROM equipo
         GROUP BY provincia
-        HAVING COUNT(*) > 5;
+        HAVING COUNT(*) > 2;
     END;
 
 -- 6. Procedimiento que muestre la relación de jugadores con su nombre y fecha de incorporación al indicar como parámetro el nombre del equipo. Ejercicio de vistas y procedimientos
+
+CREATE OR ALTER PROCEDURE pa_jugadoresEquipo
+    @equipo VARCHAR(50)
+    AS BEGIN
+        SELECT j.nombre, r.fecha
+        FROM jugador j
+        INNER JOIN reserva r ON j.id_jugador = r.id_jugador
+        INNER JOIN equipo e ON r.id_equipo = e.id_equipo
+        WHERE e.nombre = @equipo;
+    END;
+
 -- 7. Procedimiento que muestre el jugador del partido con su nombre, equipo y estadística al indicar como parámetro el código del partido, si no se introduce el código del partido se mostrará por pantalla como un error y se sale de la ejecución.
+
+CREATE OR ALTER PROCEDURE pa_jugadorPartido
+    @partido INT
+    AS BEGIN
+        IF @partido IS NULL
+        BEGIN
+            PRINT 'Error: Debe introducir un código de partido.';
+            RETURN;
+        END
+
+        SELECT j.nombre, e.nombre AS equipo, jp.faltasrecibidas, jp.faltascometidas, jp.puntos, jp.asistencias, jp.rebotes, jp.perdidas, jp.recuperacion
+        FROM jugadorpartido jp
+        INNER JOIN jugador j ON jp.jugador = j.id_jugador
+        INNER JOIN equipo e ON j.equipo = e.id_equipo
+        WHERE jp.partido = @partido;
+    END;
+
 -- 8. Cree una vista que muestre los MVP de cada jornada.
 
 CREATE OR ALTER VIEW vi_mvp_jornada AS
